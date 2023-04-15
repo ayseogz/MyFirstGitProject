82847dbe25fdddbecf780652e15360552d97b4e1
cenk.kutan <cenktunakutan@gmail.com>
2023-04-15 14:25:31 +0300

    son surum

--- MyFirstGitProject/Program.cs
+++ MyFirstGitProject/Program.cs
@@ -6,9 +6,7 @@
         {
             Console.WriteLine("hello");
             Console.WriteLine("world");
-            Console.WriteLine("hello");
-            Console.WriteLine("world");
-            Console.WriteLine("hello");
+          
             Console.WriteLine("hellosssss");
 
             Console.WriteLine("yeni");
